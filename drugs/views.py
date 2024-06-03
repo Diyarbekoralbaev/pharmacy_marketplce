893366ed418bfe4183c0cb034633577c759d53c7
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-06-04 00:20:02 +0500

    updated views.py: add class UpdateDrugView

--- drugs/views.py
+++ drugs/views.py
@@ -24,4 +24,22 @@ class CreateDrugView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors)
     
+class UpdateDrugView(APIView):
+    @swagger_auto_schema(
+        request_body=DrugUpdateSerializer,
+        responses={
+            200: openapi.Response('Drug updated successfully.'),
+            400: openapi.Response('Bad Request')
+        }
+    )
+    def put(self, request, pk):
+        try:
+            drug = Drug.objects.get(pk=pk)
+        except Drug.DoesNotExist:
+            return Response({'error': 'Drug does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = DrugUpdateSerializer(drug, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
