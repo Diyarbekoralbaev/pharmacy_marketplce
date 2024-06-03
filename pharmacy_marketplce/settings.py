3adf013eda715cd16bf822b554d891095d69ccd5
Diyarbek Oralbaev <125976666+Diyarbekoralbaev@users.noreply.github.com>
2024-06-03 20:59:07 +0500

    access token & refresh token lifetime changed

--- pharmacy_marketplce/settings.py
+++ pharmacy_marketplce/settings.py
@@ -133,6 +133,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=24),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
