b6b26c45a179009a3f0d9c7c7a1dd538f2d8bbf3
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-06-03 21:41:53 +0500

    configured model for drugs

--- drugs/models.py
+++ drugs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Drug(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='drugs/images')
+
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
