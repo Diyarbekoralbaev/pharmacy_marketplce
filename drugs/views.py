c34a81a0cef81463da36a0200a71a96d6449f58b
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-06-04 00:19:22 +0500

    updated views.py: add class CreateDrugView

--- drugs/views.py
+++ drugs/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Drug
+from .serializers import DrugSerializer, DrugUpdateSerializer
+from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 # Create your views here.
+
+class CreateDrugView(APIView):
+    @swagger_auto_schema(
+        request_body=DrugSerializer,
+        responses={
+            200: openapi.Response('Drug created successfully.'),
+            400: openapi.Response('Bad Request')
+        }
+    )
+    def post(self, request):
+        serializer = DrugSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+
