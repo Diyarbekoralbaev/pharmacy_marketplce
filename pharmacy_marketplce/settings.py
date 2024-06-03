bd9ebb1c2ce9c86c814745e15a6dddceae8392e4
Diyarbek Oralbaev <125976666+Diyarbekoralbaev@users.noreply.github.com>
2024-06-03 21:04:15 +0500

    settings.py: drf-yasg app added for swagger documentation
    urls.py: drf-yasg configured for swagger documentation

--- pharmacy_marketplce/settings.py
+++ pharmacy_marketplce/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'drf_yasg',
 
     'users',
 
--- pharmacy_marketplce/urls.py
+++ pharmacy_marketplce/urls.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Pharmacy Marketplace API",
+      default_version='v1',
+      description="An API for a pharmacy marketplace",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
