7c7e1da0a7fad18120cfdc65ec026b9397d2d0ab
Diyarbek Oralbaev <125976666+Diyarbekoralbaev@users.noreply.github.com>
2024-06-04 00:50:01 +0500

    urls for change password, forget password and reset password functions have been configured
    - changed from /create path to /signup path during user registration

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from .views import CreateUserView, LoginView, UserMeView, UserListView, UserDetailView
+from .views import CreateUserView, LoginView, UserMeView, UserListView, UserDetailView, UserChangePasswordView, UserForgotPasswordView, UserResetPasswordView
 
 urlpatterns = [
     path('', UserListView.as_view()),
     path('<int:pk>/', UserDetailView.as_view()),
     path('me/', UserMeView.as_view()),
-    path('create/', CreateUserView.as_view()),
+    path('signup/', CreateUserView.as_view()),
     path('login/', LoginView.as_view()),
+    path('change-password/', UserChangePasswordView.as_view()),
+    path('forgot-password/', UserForgotPasswordView.as_view()),
+    path('reset-password/', UserResetPasswordView.as_view()),
 ]
