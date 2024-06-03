b7293fcecb2bf21c89ee0b11552defa8e0e44f65
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-06-03 23:38:41 +0500

    created new serializer: DrugUpdateSerializer
     -valide
    
     -update

--- drugs/serializers.py
+++ drugs/serializers.py
@@ -49,3 +49,42 @@ class DrugSerializer(serializers.ModelSerializer):
             setattr(instance, attr, value)
         instance.save()
         return instance
+    
+
+class DrugUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drug
+        fields = ('id', 'name', 'description', 'price', 'image')
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'name': {'required': False},
+            'description': {'required': False},
+            'price': {'required': False},
+            'image': {'required': False},
+        }
+
+    def validate(self, data):
+        if 'price' in data:
+            price = data['price']
+            if not isinstance(price, decimal.Decimal):
+                raise serializers.ValidationError("Price must be a number.")
+            if price < 0:
+                raise serializers.ValidationError("Price must be greater than 0.")
+        
+        IMAGE_FILE_TYPES = ['png', 'jpg', 'jpeg']
+        IMAGE_MAX_SIZE = 5242880
+
+        if 'image' in data:
+            image = data['image']
+            if not image.name.endswith(tuple(IMAGE_FILE_TYPES)):
+                raise serializers.ValidationError("Image must be a PNG, JPG, or JPEG file.")
+            if image.size > IMAGE_MAX_SIZE:
+                raise serializers.ValidationError("Image must be less than 5MB.")
+        
+        return data
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
