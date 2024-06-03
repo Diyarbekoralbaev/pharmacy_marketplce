df3e8d9a07cb562e713a828e18a20a395ecc8433
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-06-03 22:30:23 +0500

    updated drug serializer: added validation for price and image

--- drugs/serializers.py
+++ drugs/serializers.py
@@ -1,3 +1,4 @@
+import decimal
 from rest_framework import serializers
 from .models import Drug
 
@@ -13,7 +14,32 @@ class DrugSerializer(serializers.ModelSerializer):
             'price': {'required': True},
             'image': {'required': True},
         }
-    
+
+    def validate(self, data):
+        if 'price' in data:
+            price = data['price']
+            if not isinstance(price, decimal.Decimal):
+                raise serializers.ValidationError("Price must be a number.")
+            if price < 0:
+                raise serializers.ValidationError("Price must be greater than 0.")
+        else:
+            raise serializers.ValidationError("Price is required.")
+        
+        IMAGE_FILE_TYPES = ['png', 'jpg', 'jpeg']
+        IMAGE_MAX_SIZE = 5242880
+
+        if 'image' in data:
+            image = data['image']
+            if not image.name.endswith(tuple(IMAGE_FILE_TYPES)):
+                raise serializers.ValidationError("Image must be a PNG, JPG, or JPEG file.")
+            if image.size > IMAGE_MAX_SIZE:
+                raise serializers.ValidationError("Image must be less than 5MB.")
+        else:
+            raise serializers.ValidationError("Image is required.")
+        
+        return data
+
+
     def create(self, validated_data):
         drug = Drug.objects.create(**validated_data)
         return drug
