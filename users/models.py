6b04731124f7e9394c62ba4598e5aba44f68f1bb
Diyarbek Oralbaev <125976666+Diyarbekoralbaev@users.noreply.github.com>
2024-06-03 18:39:40 +0500

    CustomUser model created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('seller', 'Seller'),
+        ('buyer', 'Buyer'),
+    )
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField(max_length=50, unique=True, blank=True, null=True)
+    phone = models.CharField(max_length=50, unique=True)
+    photo = models.ImageField(upload_to='users/', default='users/default.png')
+    date_joined = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=50)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='buyer')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
