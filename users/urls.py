f1a3fb764b0d76ebcb9c66cd31b24ce2fb27c06d
Diyarbek Oralbaev <125976666+Diyarbekoralbaev@users.noreply.github.com>
2024-06-04 23:27:16 +0500

    name added for each url

--- users/urls.py
+++ users/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from .views import CreateUserView, LoginView, UserMeView, UserListView, UserDetailView, UserChangePasswordView, UserForgotPasswordView, UserResetPasswordView
 
 urlpatterns = [
-    path('', UserListView.as_view()),
-    path('<int:pk>/', UserDetailView.as_view()),
+    path('', UserListView.as_view(), name='user-list'),
+    path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
     path('me/', UserMeView.as_view()),
-    path('signup/', CreateUserView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('change-password/', UserChangePasswordView.as_view()),
-    path('forgot-password/', UserForgotPasswordView.as_view()),
-    path('reset-password/', UserResetPasswordView.as_view()),
+    path('signup/', CreateUserView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('change-password/', UserChangePasswordView.as_view(), name='change-password'),
+    path('forgot-password/', UserForgotPasswordView.as_view(), name='forgot-password'),
+    path('reset-password/', UserResetPasswordView.as_view(), name='reset-password'),
 ]
