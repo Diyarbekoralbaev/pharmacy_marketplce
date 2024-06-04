a29beb3fd7e02af51e29500aeb2cda7f15046bc0
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-06-05 01:01:09 +0500

    updated DrugUpdateSerializer: add extra fields with fixing bugs

--- drugs/serializers.py
+++ drugs/serializers.py
@@ -7,10 +7,10 @@ from .models import Drug
 class DrugSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drug
-        fields = ('id', 'name', 'description', 'price', 'image', 'created_at', 'quantity', 'category', 'manufacturer_country', 'manufacturer', 'active_substance', 'type', 'dozens', 'expiration_date', 'brand')
+        fields = ('id', 'drug_name', 'description', 'price', 'image', 'created_at', 'quantity', 'category', 'manufacturer_country', 'manufacturer', 'active_substance', 'type', 'dozens', 'expiration_date', 'brand')
         extra_kwargs = {
             'id': {'read_only': True},
-            'name': {'required': True},
+            'drug_name': {'required': True},
             'description': {'required': True},
             'price': {'required': True},
             'image': {'required': True},
@@ -72,6 +72,12 @@ class DrugSerializer(serializers.ModelSerializer):
         if not value.isalpha():
             raise serializers.ValidationError("Manufacturer country must contain only letters.")
         return value   
+    
+
+    def validate_manufacturer(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("Manufacturer must contain only letters.")
+        return value
         
 
     def create(self, validated_data):
@@ -88,41 +94,81 @@ class DrugSerializer(serializers.ModelSerializer):
 class DrugUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drug
-        fields = ('id', 'name', 'description', 'price', 'image', 'quantity')
+        fields = ('id', 'drug_name', 'description', 'price', 'image', 'quantity')
         extra_kwargs = {
-            'id': {'read_only': True},
-            'name': {'required': False},
+            'drug_name': {'required': False},
             'description': {'required': False},
             'price': {'required': False},
             'image': {'required': False},
             'quantity': {'required': False},
+            'manufacturer_country': {'required': False},
+            'manufacturer': {'required': False},
+            'active_substance': {'required': False},
+            'type': {'required': False},
+            'dozens': {'required': False},
+            'expiration_date': {'required': False},
+            'brand': {'required': False},
+            'category': {'required': False},
+            
         }
 
-    def validate(self, data):
-        if 'price' in data:
-            price = data['price']
-            if not isinstance(price, decimal.Decimal):
-                raise serializers.ValidationError("Price must be a number.")
-            if price < 0:
-                raise serializers.ValidationError("Price must be greater than 0.")
-        
+    def validate_price(self, value):
+        if not isinstance(value, decimal.Decimal):
+            raise serializers.ValidationError("Price must be a number.")
+        if value < 0:
+            raise serializers.ValidationError("Price must be greater than 0.")
+        return value
+    
+
+    def validate_image(self, value):
         IMAGE_FILE_TYPES = ['png', 'jpg', 'jpeg']
         IMAGE_MAX_SIZE = 5242880
+        if not value.name.endswith(tuple(IMAGE_FILE_TYPES)):
+            raise serializers.ValidationError("Image must be a PNG, JPG, or JPEG file.")
+        if value.size > IMAGE_MAX_SIZE:
+            raise serializers.ValidationError("Image must be less than 5MB.")
+        return value
+    
 
-        if 'image' in data:
-            image = data['image']
-            if not image.name.endswith(tuple(IMAGE_FILE_TYPES)):
-                raise serializers.ValidationError("Image must be a PNG, JPG, or JPEG file.")
-            if image.size > IMAGE_MAX_SIZE:
-                raise serializers.ValidationError("Image must be less than 5MB.")
-        
-        if 'quantity' in data:
-            quantity = data['quantity']
-            if not isinstance(quantity, int):
-                raise serializers.ValidationError("Quantity must be an integer.")
-            if quantity < 0:
-                raise serializers.ValidationError("Quantity must be greater than 0.")
-        return data
+    def validate_quantity(self, value):
+        if not isinstance(value, int):
+            raise serializers.ValidationError("Quantity must be an integer.")
+        if value < 0:
+            raise serializers.ValidationError("Quantity must be greater than 0.")
+        return value
+    
+
+    def validate_dozens(self, value):
+        if not isinstance(value, int):
+            raise serializers.ValidationError("Dozens must be an integer.")
+        if value < 0:
+            raise serializers.ValidationError("Dozens must be greater than 0.")
+        return value
+    
+
+    def validate_manufacturer_country(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("Manufacturer country must contain only letters.")
+        return value
+    
+
+    def validate_manufacturer(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("Manufacturer must contain only letters.")
+        return value
+    
+
+    def validate_expiration_date(self, value):
+        if value < datetime.date.today():
+            raise serializers.ValidationError("Expiration date must be greater than today.")
+        return value
+    
+
+    def validate_category(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("Category must contain only letters.")
+        return value
+    
 
     def update(self, instance, validated_data):
         for attr, value in validated_data.items():
