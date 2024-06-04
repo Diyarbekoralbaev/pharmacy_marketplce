e17dd72470f7f2be783b9fcf37fbd6ec0a8c2dbe
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-06-04 23:57:46 +0500

    updated Drug class: fixed bugs with blanks and nullable values

--- drugs/models.py
+++ drugs/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 
 class Drug(models.Model):
-    drug_name = models.CharField(max_length=100, null=False, blank=False,)
-    description = models.TextField(null=False, blank=False,)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False,)
-    image = models.ImageField(upload_to='images/drugs', null=False, blank=False, default='images/drugs/default.jpg')
+    drug_name = models.CharField(max_length=100)
+    description = models.TextField(blank=False,)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='images/drugs', default='images/drugs/default.jpg')
     created_at = models.DateTimeField(auto_now_add=True)
-    quantity = models.IntegerField(default=0, null=False, blank=False,)
-    expiration_date = models.DateField(null=False, blank=False,)
+    quantity = models.IntegerField(default=0)
+    expiration_date = models.DateField(blank=False,)
     brand = models.CharField(max_length=100)
-    category = models.CharField(max_length=100, null=False, blank=False,)
-    manufacturer_country = models.CharField(max_length=100, null=False, blank=False,)
-    manufacturer = models.CharField(max_length=100, null=False, blank=False,)
-    active_substance = models.CharField(max_length=100, null=False, blank=False,)
+    category = models.CharField(max_length=100, blank=False,)
+    manufacturer_country = models.CharField(max_length=100)
+    manufacturer = models.CharField(max_length=100)
+    active_substance = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
-    dozens = models.IntegerField(default=0, null=False, blank=False,)
+    dozens = models.IntegerField(default=0, blank=False,)
 
     def __str__(self):
         return self.drug_name
