9f0ad60e79569d5520fefba4a7ebf1d6b88f0ac9
Diyarbek Oralbaev <125976666+Diyarbekoralbaev@users.noreply.github.com>
2024-06-05 17:21:49 +0500

    urls configured for OrderListCreateView and OrderDetailView

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import CreateUserView, LoginView, UserMeView, UserListView, UserDetailView, UserChangePasswordView, UserForgotPasswordView, UserResetPasswordView
+from .views import CreateUserView, LoginView, UserMeView, UserListView, UserDetailView, UserChangePasswordView, UserForgotPasswordView, UserResetPasswordView, \
+    OrderListCreateView, OrderDetailView
 
 urlpatterns = [
     path('', UserListView.as_view(), name='user-list'),
@@ -11,4 +12,7 @@ urlpatterns = [
     path('change-password/', UserChangePasswordView.as_view(), name='change-password'),
     path('forgot-password/', UserForgotPasswordView.as_view(), name='forgot-password'),
     path('reset-password/', UserResetPasswordView.as_view(), name='reset-password'),
+
+    path('orders/', OrderListCreateView.as_view(), name='order-list-create'),
+    path('orders/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
 ]
