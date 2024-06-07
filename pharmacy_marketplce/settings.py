33dd1b037e35ffa7e8468bb6cdc840f2c744c7f2
Diyarbek Oralbaev <125976666+Diyarbekoralbaev@users.noreply.github.com>
2024-06-07 05:03:14 +0500

    trying to fix cors error

--- pharmacy_marketplce/settings.py
+++ pharmacy_marketplce/settings.py
@@ -163,9 +163,5 @@ CACHES = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://pdp.araltech.tech",
-    "https://pdp.araltech.tech",
-    "http://localhost:3000",
-    "http://localhost:8000",
-]
+
+CORS_ALLOW_CREDENTIALS = True
