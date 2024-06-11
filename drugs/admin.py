0568920ff0aa37cc2edc49c606f3992dea9abeaf
Diyarbek Oralbaev <125976666+Diyarbekoralbaev@users.noreply.github.com>
2024-06-11 08:24:27 +0500

    django admin configured for each model

--- drugs/admin.py
+++ drugs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Drug
 
-# Register your models here.
+
+class DrugAdmin(admin.ModelAdmin):
+    list_display = ['drug_name', 'price', 'quantity', 'expiration_date', 'category', 'seller']
+    list_filter = ['category', 'seller']
+    search_fields = ['drug_name', 'category', 'seller']
+
+
+admin.site.register(Drug, DrugAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import CustomUser, OrderModel, OrderItemModel
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'role', 'is_active', 'is_staff']
+    search_fields = ['username', 'first_name', 'last_name', 'phone']
+    list_filter = ['role', 'is_active', 'is_staff']
+    list_per_page = 10
+    ordering = ['username']
+
+
+class OrderModelAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at', 'status', 'total_price']
+    search_fields = ['user__username', 'user__first_name', 'user__last_name', 'user__phone']
+    list_filter = ['status']
+    list_per_page = 10
+    ordering = ['-created_at']
+
+
+class OrderItemModelAdmin(admin.ModelAdmin):
+    list_display = ['order', 'drug', 'quantity', 'price']
+    search_fields = ['order__user__username', 'order__user__first_name', 'order__user__last_name', 'order__user__phone']
+    list_per_page = 10
+    ordering = ['order']
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(OrderModel, OrderModelAdmin)
+admin.site.register(OrderItemModel, OrderItemModelAdmin)
